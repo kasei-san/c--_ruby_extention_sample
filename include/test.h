180f888c9e4abbd47f04af1a9860d8b8a17f9e27
kasei-san <kasei.san@gmail.com>
2014-04-08 09:47:43 +0900

    add nemespace Mylib

--- include/test.h
+++ include/test.h
@@ -1,10 +1,12 @@
 #ifndef __TEST_H__
 #define __TEST_H__
 
-class Test{
-  public:
-    Test();
-    ~Test();
-    void sample_method();
-};
+namespace MyLib {
+  class Test{
+    public:
+      Test();
+      ~Test();
+      void sample_method();
+  };
+}
 #endif
--- src/test.cpp
+++ src/test.cpp
@@ -1,13 +1,13 @@
 #include <test.h>
 #include <iostream>
 
-Test::Test(){ std::cout << "call constructor" << std::endl; }
-Test::~Test(){ std::cout << "calll destructor" << std::endl; }
-void Test::sample_method(){ std::cout << "call sample_method" << std::endl; }
+MyLib::Test::Test(){ std::cout << "call constructor" << std::endl; }
+MyLib::Test::~Test(){ std::cout << "calll destructor" << std::endl; }
+void MyLib::Test::sample_method(){ std::cout << "call sample_method" << std::endl; }
 
 /* 動作確認用 */
 int main(){
-  Test* test = new Test();
+  MyLib::Test* test = new MyLib::Test();
   test->sample_method();
   delete test;
 }
